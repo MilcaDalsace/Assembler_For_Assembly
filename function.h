86fd647abb1b9a9ebf7e584f49ac1b0db6cfd070
MilcaDalsace <milcad105@gmail.com>
2024-08-21 16:07:15 +0300

    function comment

--- function.h
+++ function.h
@@ -17,7 +17,8 @@ extern const char ZERO[];
 extern const char NOT_FOUND[];
 extern const int registers[];
 
-typedef struct {
+typedef struct
+{
     char symbol[MAX_LINE_LENGTH];
     char code[CODE_SEGMENT_SIZE];
     char externName[MAX_LABEL_NAME];
@@ -25,27 +26,30 @@ typedef struct {
     int isData;
     int isEntry;
     int isExtern;
-}Symbol;
+} Symbol;
 
 extern Symbol *symbols;
 extern int symbolCount;
 
-typedef struct {
+typedef struct
+{
     char *name;
     int count;
-}SymbolTable;
+} SymbolTable;
 
 extern SymbolTable *symbolTable;
 extern int labelCount;
 
-typedef struct {
+typedef struct
+{
     char *name;
-}External;
+} External;
 
 External *externs;
 extern int externCount;
 
-typedef struct {
+typedef struct
+{
     int wordCounter;
 } L;
 extern L *l;
@@ -53,85 +57,97 @@ extern L *l;
 extern int IC;
 extern int DC;
 
-int addData(const char *line, int countLine,const char * newSymbolName);
+/* Adds data to the symbol table.*/
+int addData(const char *line, int countLine, const char *newSymbolName);
 
+/* Adds a string to the symbol table*/
 int addString(const char *line, int countLine, const char *newSymbolName);
 
+/* Processes an external definition line.*/
 int externDefinition(const char *line, int countLine);
 
+/* Processes an entry definition line.*/
 int entryDefinition(const char *line, int countLine, int isFirstPass);
 
-int addOperation(const char *line,int numOper, int countLine, const char *newSymbolName);
+/* Adds an operation to the symbol table based on the given line and operation number.*/
+int addOperation(const char *line, int numOper, int countLine, const char *newSymbolName);
 
+/* Updates the operands in the symbol table based on the given line and address.*/
 int updateOparand(const char *line, int countLine, int countAdress);
 
-char *changeNameOfFile(const char *sourceFileName,const char *fileNameEnding);
+/* Changes the file name extension to the specified ending.*/
+char *changeNameOfFile(const char *sourceFileName, const char *fileNameEnding);
 
-int operationWithTwoOperand(const char *line,const char* newSymbolName, int numOper, int countLine);
+/* Performs an operation with two operands.*/
+int operationWithTwoOperand(const char *line, const char *newSymbolName, int numOper, int countLine);
 
+/* Performs an operation with one operand.*/
 int operationWithOneOperand(const char *line, const char *newSymbolName, int numOper, int countLine);
 
+/* Performs an operation without operands.*/
 int operationWithoutOperand(const char *line, const char *newSymbolName, int numOper, int countLine);
 
 /*int checkIfLabbleIsData(const char* label);*/
 
-/*check if it a num*/
-int isNumber(const char *str);
+/* Checks if a label refers to a data location. */
+int checkIfLabelIsData(const char *label);
 
-/*check if it is name restricted*/
+/* Checks if a name is a restricted keyword.*/
 int isNameRestricted(const char *name);
 
-/*check if it register*/
-int isRegister( const char *name);
+/* Finds the index of a given operation in the list of operations.*/
+int findOperation(const char *name);
 
-/*find method addressing*/
-char* findsMethodOfAddressing(const char *operand);
+/* Checks if a name is a register.*/
+int isRegister(const char *name);
 
-/*miun operation*/
-char* miunOperation(int num, const char *operand1, const char *operand2);
+/* Determines the addressing method for a given operand.*/
+char *findsMethodOfAddressing(const char *operand);
 
-/*miun operand*/
-char* miunOperand(const char *operand, int firstOperand);
+/* Performs a subtraction operation on two operands and returns the corresponding machine code.*/
+char *miunOperation(int num, const char *operand1, const char *operand2);
 
-/*iun two registers*/
-char* miunTwoRegister(const char *operand1, const char *operand2);
+/* Generates machine code for a subtraction operation with a single operand.*/
+char *miunOperand(const char *operand, int firstOperand);
 
-/*add symbol*/
+/* Subtracts two registers and returns the corresponding machine code.*/
+char *miunTwoRegister(const char *operand1, const char *operand2);
+
+/* Adds a symbol to the symbol table.*/
 void addSymbol(const char *symbolName, const char *externName, const char *code, const int isData, const int isEntry, const int isExtern);
 
-/*add labbel*/
+/* Adds a label to the symbol table.*/
 void addLabel(const char *name, const int count);
 
-/*add extern*/
+/* Adds an external symbol to the list of external symbols.*/
 void addExtern(const char *name);
 
-/*add L*/
+/* Adds a line count to the list of line counts.*/
 void addL(int counteLine, int wordCounter);
 
 /*check if it a labbel definition*/
-char* labelDefinition(const char *token);
+char *labelDefinition(const char *token);
 
-/*find labbel*/
+/* Finds a label in the symbol table by its name.*/
 int findLabel(const char *token);
 
-/*find extern*/
-char* findExtern(const char *externName);
+/* Finds an external symbol by its name.*/
+char *findExtern(const char *externName);
 
-/*is restricted name*/
-int isNameRestricted(const char *token);
-
-/*find operation*/
-int findOperation(const char *token);
-
-/*binary to decimal*/
-int binaryToDecimal(const char *binary);
-
-/*decimal to binary*/
-char* decimalToBinary(int n, int numOfChar);
-
+/* Checks if a string is enclosed in double quotes.*/
 int isCorrectString(const char *name);
 
-/*check if the datas write correct*/
+/* Checks if a string represents a valid number.*/
+int isNumber(const char *str);
+
+/* Converts a binary string to its decimal equivalent.*/
+int binaryToDecimal(const char *binary);
+
+/* Converts a decimal number to its binary representation.*/
+char *decimalToBinary(int n, int numOfChar);
+
+/* Correct commas in a string representing a list of objects.*/
 int correctCommas(char *line);
 
+/* The function frees the defined memory */
 void freeMemory();
